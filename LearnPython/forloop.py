ee33d7118fea7adecff802bc0b535173c7eb827c
Roli Joshi <joshi.roli@gmail.com>
2019-06-02 10:12:29 +0200

    loops

---
+++ LearnPython/forloop.py
@@ -0,0 +1,24 @@
+# primes = [2,3,4,6,7]
+# for prime in primes:
+#     print(prime)
+#
+# for x in range(0,10,2):
+#     print (x)
+
+count = 0
+# while count <5 :
+#     print(count)
+#     count=count+1
+
+while True:
+    print(count)
+    count += 1
+    if count < 6:
+        print(count)
+        break
+for y in range(10):
+    if y % 2 == 0:
+        continue
+    print(y)
+
+
--- LearnPython/string_formatiing.py
+++ LearnPython/string_formatiing.py
@@ -1,4 +1,16 @@
 data = ("John", "Doe", 53.44)
 format_string = "Hello %s %s. Your current balance is $%s."
+print(format_string[3:10:4])
+print(format_string[3:10])
 
 print(format_string % data)
+
+astring = "Roli Bhuwan Joshi"
+print(astring[::-1])
+print(astring.upper())
+print(astring.lower())
+print(astring.startswith("Roli"))
+print(astring.endswith("hi"))
+print(astring.split("Bhu"))
+print(astring[0::2])
+
